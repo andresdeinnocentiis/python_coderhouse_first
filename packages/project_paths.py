36e733d58bf695a9ecad4fb228cb8141dcacd67e
andresdeinnocentiis <andresdeinnocentiis@gmail.com>
2022-12-26 18:17:43 -0300

    Final

--- packages/project_paths.py
+++ packages/project_paths.py
@@ -14,4 +14,4 @@ DATABASE_FILE_CSV = os.path.join(BASE_DIR, "users_db.csv")
 CAMBIAR EL PATH DE ACÁ ABAJO POR ALGUNO DE LOS 3 DE ARRIBA 'DATABASE', segun se quiera realizar el proceso con un archivo .txt, .json o .csv.    
 """
 
-FILE_PATH = DATABASE_FILE_CSV
+FILE_PATH = DATABASE_FILE_JSON
